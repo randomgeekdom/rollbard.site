52910103e9e5f791bccb12c170d4f3130da81ed4
randomgeekdom <randomgeekdom@gmail.com>
2022-12-11 18:30:13 -0600

    Getting character generator up and running

--- src/pages/Fate-Core/Character-Generator.vue
+++ src/pages/Fate-Core/Character-Generator.vue
@@ -1,11 +1,25 @@
 
+<script setup lang="ts">
+  import { NPC, NPCGenerator, Randomizer } from "@randomgeekdom/rollbard";
+  import{ref, Ref} from "vue";
+  
+  let npcGenerator = new NPCGenerator(new Randomizer());
+
+  let character: Ref<NPC> = ref(npcGenerator.Generate());  
+  
+  let GenerateCharacter = function()
+  {
+    character.value = npcGenerator.Generate();
+  }
+
+</script>
 
 <template>
-
   <div class="flex items-center mt-8 intro-y">
-    <h2 class="mr-auto text-lg font-medium">Page 2</h2>
+    <button @click="GenerateCharacter()">Generate</button>
   </div>
   <!-- BEGIN: Page Layout -->
-  <div class="p-5 mt-5 intro-y box">Example page 2</div>
+  <div class="p-5 mt-5 intro-y box">{{ character?.Name }}</div>
   <!-- END: Page Layout -->
 </template>
+
