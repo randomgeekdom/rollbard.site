d4d16ecfa4e0e4744b544aac5d59ba8103f92e1e
randomgeekdom <randomgeekdom@gmail.com>
2022-12-12 21:33:09 -0600

    Creating a model for fate core characters

--- src/pages/Fate-Core/Character-Generator.vue
+++ src/pages/Fate-Core/Character-Generator.vue
@@ -1,18 +1,23 @@
 <script setup lang="ts">
 import { NPC, NPCGenerator, Randomizer, Gender } from "@randomgeekdom/rollbard";
 import { ref, Ref } from "vue";
+import FateCoreCharacter from "../../models/FateCoreCharacter";
 
 let randomizer = new Randomizer();
 let npcGenerator = new NPCGenerator(randomizer);
 
-let character: Ref<NPC> = ref(npcGenerator.Generate());
+let character: Ref<FateCoreCharacter> = ref(new FateCoreCharacter());
 
 let GenerateCharacter = function () {
-  character.value = npcGenerator.Generate();
+  var npc = npcGenerator.Generate();
+  character.value = {
+    Name: npc.Name,
+    Pronouns: GetPronoun(npc.Gender),
+  };
 };
 
-let GetPronoun = function(gender: Gender): string{
-  switch(gender){
+let GetPronoun = function (gender: Gender): string {
+  switch (gender) {
     case Gender.Female:
       return "She/Her";
     case Gender.Male:
@@ -20,7 +25,9 @@ let GetPronoun = function(gender: Gender): string{
     default:
       return "They/Them";
   }
-}
+};
+
+GenerateCharacter();
 </script>
 
 <template>
@@ -30,12 +37,8 @@ let GetPronoun = function(gender: Gender): string{
   <!-- BEGIN: Page Layout -->
   <div class="p-5 mt-5 intro-y box">
     <form>
-      <div>
-        Name: {{ character?.Name }}
-      </div>
-      <div>
-        Pronouns: {{GetPronoun(character.Gender)}}
-      </div>
+      <div>Name: {{ character?.Name }}</div>
+      <div>Pronouns: {{ character?.Pronouns }}</div>
     </form>
   </div>
   <!-- END: Page Layout -->
