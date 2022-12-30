49743bc02e794b527edae4793fa3de792934d9ce
randomgeekdom <randomgeekdom@gmail.com>
2022-12-30 16:55:36 -0600

    adding auth0

--- src/components/TopBar/TopBar.vue
+++ src/components/TopBar/TopBar.vue
@@ -8,6 +8,8 @@ import fakerData from "../../utils/faker";
 import _ from "lodash";
 import { TransitionRoot } from "@headlessui/vue";
 import RouteInfo from "../../types/RouteInfo";
+import { useAuth0 } from "@auth0/auth0-vue";
+import logoUrl from "../../assets/images/logo.svg";
 
 const searchDropdown = ref(false);
 const showSearchDropdown = () => {
@@ -18,50 +20,53 @@ const hideSearchDropdown = () => {
 };
 
 const props = defineProps<{
-  routes: RouteInfo[]; 
-}>()
+  routes: RouteInfo[];
+}>();
 
+const { loginWithRedirect, logout, isAuthenticated, user } = useAuth0();
 </script>
 
 <template>
   <!-- BEGIN: Top Bar -->
   <div class="h-[67px] z-[51] flex items-center relative border-b border-slate-200">
-  
-    <button class="mr-auto -intro-x sm:flex" v-for="route in routes"  :to="'{name:' + route.name +'}'">
-      {{route.display}}
+    <button
+      class="mr-auto -intro-x sm:flex"
+      v-for="route in routes"
+      :to="'{name:' + route.name + '}'"
+    >
+      {{ route.display }}
     </button>
     <!-- END: Breadcrumb -->
-      
+
     <!-- BEGIN: Account Menu -->
     <Menu>
       <Menu.Button
         class="block w-8 h-8 overflow-hidden rounded-full shadow-lg image-fit zoom-in intro-x"
       >
-        <img alt="Midone Tailwind HTML Admin Template" :src="fakerData[9].photos[0]" />
+        <img
+          alt="Midone Tailwind HTML Admin Template"
+          v-if="isAuthenticated"
+          :src="user?.picture"
+        />
+        <Lucide icon="Lock" v-if="!isAuthenticated" />
       </Menu.Button>
       <Menu.Items class="w-56 mt-px text-white bg-primary">
-        <Menu.Header class="font-normal">
-          <div class="font-medium">{{ fakerData[0].users[0].name }}</div>
+        <Menu.Header class="font-normal" v-if="isAuthenticated">
+          <div class="font-medium">{{ user?.name }}</div>
           <div class="text-xs text-white/70 mt-0.5 dark:text-slate-500">
-            {{ fakerData[0].jobs[0] }}
+            {{ user?.email }}
           </div>
         </Menu.Header>
         <Menu.Devider class="bg-white/[0.08]" />
-        <Menu.Item class="hover:bg-white/5">
-          <Lucide icon="User" class="w-4 h-4 mr-2" /> Profile
+        <Menu.Item class="hover:bg-white/5" @click="logout()" v-if="isAuthenticated">
+          <Lucide icon="ToggleLeft" class="w-4 h-4 mr-2" /> Logout
         </Menu.Item>
-        <Menu.Item class="hover:bg-white/5">
-          <Lucide icon="Edit" class="w-4 h-4 mr-2" /> Add Account
-        </Menu.Item>
-        <Menu.Item class="hover:bg-white/5">
-          <Lucide icon="Lock" class="w-4 h-4 mr-2" /> Reset Password
-        </Menu.Item>
-        <Menu.Item class="hover:bg-white/5">
-          <Lucide icon="HelpCircle" class="w-4 h-4 mr-2" /> Help
-        </Menu.Item>
-        <Menu.Devider class="bg-white/[0.08]" />
-        <Menu.Item class="hover:bg-white/5">
-          <Lucide icon="ToggleRight" class="w-4 h-4 mr-2" /> Logout
+        <Menu.Item
+          class="hover:bg-white/5"
+          @click="loginWithRedirect()"
+          v-if="!isAuthenticated"
+        >
+          <Lucide icon="ToggleRight" class="w-4 h-4 mr-2" /> Login
         </Menu.Item>
       </Menu.Items>
     </Menu>
--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,12 @@ import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./router";
 import "./assets/css/app.css";
+import { createAuth0 } from '@auth0/auth0-vue';
 
-createApp(App).use(router).use(createPinia()).mount('#app')
+createApp(App).use(router).use(createPinia()).use(
+    createAuth0({
+      domain: "storysuite-auth.us.auth0.com",
+      client_id: "jTEB1v1oxIEe46mo3uabBJlV4uB0hBsV",
+      redirect_uri: window.location.origin
+    })
+  ).mount('#app')
