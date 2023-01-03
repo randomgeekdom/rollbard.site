c3834a33988b1472c6e4960a9c74790d51d60945
randomgeekdom <randomgeekdom@gmail.com>
2023-01-02 19:42:33 -0600

    Setting a dev and prod api url

--- src/services/ApiService.ts
+++ src/services/ApiService.ts
@@ -1,12 +1,16 @@
 import { useAuth0 } from "@auth0/auth0-vue";
 import { IApiService } from "./interfaces/IApiService";
 import settingsJson from "./settings.json";
+import { defineConfig, loadEnv } from 'vite';
 
 export default class ApiService implements IApiService {
+  apiUrl: string;
+  isDev = false;
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   auth: any;
   constructor() {
     this.auth = useAuth0();
+    this.apiUrl = this.isDev ? settingsJson.DevApiUrl : settingsJson.ApiUrl;
   }
   async GetAsync(url: string): Promise<Response> {
     const token = await this.auth.getAccessTokenSilently();
@@ -18,7 +22,7 @@ export default class ApiService implements IApiService {
       },
     };
 
-    return fetch(settingsJson.ApiUrl + url, requestOptions);
+    return fetch(this.apiUrl + url, requestOptions);
   }
 
   async PostAsync<T>(url: string, body: T): Promise<Response> {
@@ -32,6 +36,6 @@ export default class ApiService implements IApiService {
       body: JSON.stringify(body),
     };
 
-    return fetch(settingsJson.ApiUrl + url, requestOptions);
+    return fetch(this.apiUrl + url, requestOptions);
   }
 }
